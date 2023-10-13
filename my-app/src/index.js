eb20cd6db6929f8689a8396cdb4bb7ee96ad2305
Jaren Pinera <jpinera@hawaii.edu>
2023-10-12 21:21:59 -1000

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,21 +9,25 @@ import { Yelp, Instagram, Envelope} from 'react-bootstrap-icons';
 // 1. Top Menu
 const TopMenu = () => (
   <Navbar expand="lg" className="py-4 change-font">
-    <Container>
+    <Container className="py-2">
       <Navbar.Brand href="#home"><Image src="/hanatea-logo.png" alt="logo" height={50}/></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Nav activeKey="/home" className="fs-5 justify-content-center">
-        <Nav.Link className="px-0 pb-0 mx-2" href="/home">Home</Nav.Link>
-        <Nav.Link href="#">Menu</Nav.Link>
-        <Nav.Link href="#">About</Nav.Link>
-        <Nav.Link href="#">Visit</Nav.Link>
-        <Nav.Link href="#">Join our team</Nav.Link>
-      </Nav>
-      <Nav className="fs-5 justify-content-end">
-        <Nav.Link href="#" className="me-2"><Instagram/></Nav.Link>
-        <Nav.Link href="#" className="me-2"><Yelp/></Nav.Link>
-        <Nav.Link href="#"><Envelope/></Nav.Link>
-      </Nav>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav activeKey="/home" className="fs-5 justify-content-end">
+          <Nav.Link className="px-0 pb-0 me-2" href="/home">Home</Nav.Link>
+          <Nav.Link href="#">Menu</Nav.Link>
+          <Nav.Link href="#">About</Nav.Link>
+          <Nav.Link href="#">Visit</Nav.Link>
+          <Nav.Link href="#">Join our team</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav className="fs-5 justify-content-end">
+          <Nav.Link href="#" className="me-2"><Instagram/></Nav.Link>
+          <Nav.Link href="#" className="me-2"><Yelp/></Nav.Link>
+          <Nav.Link href="#"><Envelope/></Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Container>
   </Navbar>
 );
@@ -32,7 +36,7 @@ const TopMenu = () => (
 const BodyImageText = () => (
   <Card className="text-white">
     <Image src="/hanatea-backdrop-edit.jpeg" className="card-img" alt="Backdrop Image"/>
-    <div className="card-img-overlay">
+    <Container fluid className="card-img-overlay">
       <Container fluid className="row align-content-center justify-content-end" id="mainText">
         <Col className="text-end me-3">
           <h1 className="display-2 mb-4"><strong>Dillingham Hours:</strong></h1>
@@ -45,32 +49,31 @@ const BodyImageText = () => (
           <Button type="button" variant="light" size="lg" className="py-3 px-4">Pearl City Order</Button>
         </Col>
       </Container>
-    </div>
+    </Container>
   </Card>
 );
 
 // 3. Special Menu text
 const SpecialMenu = () => (
   <Row className="align-content-center justify-content-center pt-5" id="middle">
-    <Col lg={10} className="text-center py-5">
-      <div className="p-1 change-font">
+    <Col lg={10} className="text-center py-5 mb-5">
+      <Container className="p-1 change-font">
         <h5 className="mb-4">During special hours, only certain drinks are available.</h5>
         <h3 className="my-5">‖   In-store   and   online   ordering   available   ‖</h3>
         <h5 className="my-4 change-font">We accept phone order. Please call (808)376-8139.</h5>
-      </div>
-      <div className="p-1">
+      </Container>
+      <Container className="p-1">
         <Button type="button" variant="dark" className="py-3 px-4">Special Menu</Button>
-      </div>
+      </Container>
     </Col>
-    <Nav  activeKey="/home" className="justify-content-center ms-4">
-
-    </Nav>
-    <Nav>
-      <Nav.Link className="px-0 pb-0 mx-2" href="/home">Home</Nav.Link>
-      <Nav.Link href="#">Menu</Nav.Link>
-      <Nav.Link href="#">About</Nav.Link>
-      <Nav.Link href="#">Visit</Nav.Link>
-      <Nav.Link href="#">Join our team</Nav.Link>
+    <Nav activeKey="/home" className="justify-content-center ms-4 mt-5 py-5">
+      <Nav.Item className="fs-4 mx-3"><Nav.Link className="p-0" href="/home" aria-current="page">Home</Nav.Link></Nav.Item>
+      <div className="fs-3">||</div>
+      <Nav.Item className="fs-4 mx-3"><Nav.Link className="p-0" href="/home">Special Menu</Nav.Link></Nav.Item>
+      <div className="fs-3">||</div>
+      <Nav.Item className="fs-4 mx-3"><Nav.Link className="p-0" href="/home">About</Nav.Link></Nav.Item>
+      <div className="fs-3">||</div>
+      <Nav.Item className="fs-4 mx-3"><Nav.Link className="p-0" href="/home">Visit</Nav.Link></Nav.Item>
     </Nav>
   </Row>
 );
